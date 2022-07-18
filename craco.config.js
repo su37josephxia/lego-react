2727fd8aa01e245e19c6935effea0913acd55147
josephxia <15906475@qq.com>
2022-07-18 20:18:57 +0800

    update

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,24 @@
 // * 配置完成后记得重启下
 const CracoLessPlugin = require("craco-less");
-
+const path = require('path')
 module.exports = {
+  webpack: {
+    alias: {
+    },
+    // 以下代码！！！  与alias或babel同级
+    configure:(webpackConfig,{env,paths}) => {
+      // 修改build的生成文件名称
+      paths.appBuild = 'docs';
+      webpackConfig.output ={
+        ...webpackConfig.output,
+        path:path.resolve(__dirname,'docs'),
+        publicPath:'./'
+      }
+      return webpackConfig;
+    },
+  },
+
+
   babel: {
     //用来支持装饰器
     plugins: [["@babel/plugin-proposal-decorators", {legacy: true}]],
